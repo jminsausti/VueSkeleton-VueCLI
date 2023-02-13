64b745e5ddc084b476bea262ef69111209d5e52d
jose <jminsausti@fptxurdinaga.com>
2023-02-13 21:25:00 +0100

    ayuda axios eliminada

--- src/views/EjemploAxios.vue
+++ src/views/EjemploAxios.vue
@@ -5,25 +5,19 @@
 <script>
   import axios from 'axios';
   export default {
-      
-      data(){
-        return{
+    data(){
+      return{
           
         }
       },
-      mounted() {
-          this.cargarUsuarios();
-      },
+    mounted(){
+      this.cargarUsuarios();
+    },
     methods:{
       cargarUsuarios() {
-              
-              axios.get('../json/Usuarios.json')
-                  .then((respuesta) => {
-                      console.log(respuesta);
-                      console.log(respuesta.data);
-                  });
-          }
+        
       }
     }
+  }
 </script>
   
